4f1efe4af769044069e3ce88af1aa6a897ee6303
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-07-29 15:50:43 -0400

    loaded env before starting

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI || 'mongodb+srv://elijahally:jEnFeNkaACqAgQcm@cluster0.hiuzzbf.mongodb.net',
-    mongoURIWithDB: (dbString) => process.env.MONGO_URI ? (
-        process.env.MONGO_URI.split('/?')[0] + dbString + '/?' + process.env.MONGO_URI.split('/?')[1]
-    ) : (
-            `mongodb+srv://elijahally:jEnFeNkaACqAgQcm@cluster0.hiuzzbf.mongodb.net/${dbString}`
-    ),
+    mongoURIWithDB: (dbString) => {
+        const [baseUri, queryParams] =  process.env.MONGO_URI.split('/?');
+        return `${baseUri}/${dbString}?${queryParams}`;
+    },
     secretOrKey: process.env.SECRET_OR_KEY || 'and198!390QE8Wlkji',
     uploadDb: process.env.UPLOAD_DB || 'palestine_files',
     userDb: process.env.USER_DB || 'user',
